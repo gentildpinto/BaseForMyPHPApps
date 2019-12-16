fe11ad58cf7eea57b833f2c286ba16c9eab48482
Gentil Pinto <gentil.junior.gp@gmail.com>
2019-12-15 20:35:09 -0500

    update Route class

--- app/classes/Routes.php
+++ app/classes/Routes.php
@@ -5,9 +5,9 @@ namespace app\classes;
 use Exception;
 
 class Routes {
-    public static function load($routes, $url) {
-        if(!array_key_exists($url, $routes))
-            throw new Exception("Route not found {$url}");
-        return "../app/{$routes[$url]}.php";
+    public static function load($routes, $uri) {
+        if(!array_key_exists($uri, $routes))
+            throw new Exception("Route not found {$uri}");
+        return "../app/{$routes[$uri]}.php";
     }
 }
