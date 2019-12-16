d15cd2859c470861e293faa7fe5d3cd978a6103f
Gentil Pinto <gentil.junior.gp@gmail.com>
2019-12-15 20:27:18 -0500

    Layout class

---
+++ app/classes/Layout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\classes;
+
+class Layout {
+
+    private $view;
+
+    public function addLayout($view) {
+        $this->view = $view;
+    }
+
+    public function load() {
+        return "../app/views/{$this->view}.php";
+    }
+
+    public function masterLayout($view) {
+        return "../app/views/{$view}.php";
+    }
+}
