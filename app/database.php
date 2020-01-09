45f0abe28a0782404c0816d457474ce7e027a45a
Gentil Pinto <gentil.junior.gp@gmail.com>
2020-01-09 10:12:42 -0500

    minutos antes do git push

--- app/database.php
+++ app/database.php
@@ -19,7 +19,7 @@ return [
         "dbname"   => "base",
         "charset"  => "utf8",
         "user"     => "root",
-        "password" => "luffy",
+        "password" => "root",
         "collation" => "utf8_unicode_ci"
     ]
 ];
