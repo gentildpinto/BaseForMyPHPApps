9200e624096bab8013977a1c7557ba1247982738
Gentil Pinto <gentil.junior.gp@gmail.com>
2020-01-27 16:34:04 +0100

    configurations

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
         "dbname"   => "base",
         "charset"  => "utf8",
         "user"     => "root",
-        "password" => "root",
+        "password" => "",
         "collation" => "utf8_unicode_ci"
     ]
 ];
--- config/routes.php
+++ config/routes.php
@@ -2,4 +2,11 @@
 
 $route[] = ['/', 'HomeController@index'];
 $route[] = ['/posts', 'PostController@index'];
+$route[] = ['/post/{id}/show', 'PostController@show'];
+$route[] = ['/post/create', 'PostController@create'];
+$route[] = ['/post/store', 'PostController@store'];
+$route[] = ['/post/{id}/edit', 'PostController@edit'];
+$route[] = ['/post/update', 'PostController@update'];
+$route[] = ['/post/{id}/delete', 'PostController@delete'];
+
 return $route;
